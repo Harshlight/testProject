def4da18366453450bce10f0e5d85058c24292d7
JungHo <jhlove6591@naver.com>
2022-03-04 17:48:50 +0900

    this is test

--- src/test.java
+++ src/test.java
@@ -1,5 +1,8 @@
 public class test {
     public static void main (String[]args){
-        System.out.println("test");
+        int a = 1;
+        int b = 3;
+        int sum = a+b;
+        System.out.println(sum);
     }
 }
